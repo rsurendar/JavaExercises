1836ffc28459c733960d288c80538e018bc55534
rsurendar <rsurendar26@gmail.com>
2021-06-07 20:47:27 +0530

    java: non-static variable x cannot be referenced from a static context

--- src/com/royal/VariableExercise.java
+++ src/com/royal/VariableExercise.java
@@ -2,7 +2,13 @@ package com.royal;
 
 public class VariableExercise {
 
+    /*compile time error
+     * java: non-static variable x cannot be referenced from a static context
+     * */
+    init x = 10;
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(x);
+
     }
 }
